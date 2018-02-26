7c8183d44ee9cdfe1d4efe03a673a1b2c6292625
liqihua <iamliqihua@qq.com>
2018-02-26 18:13:30 +0800

    liqihua

--- src/main/java/com/liqihua/Run.java
+++ src/main/java/com/liqihua/Run.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableHystrix//启用断路器
 @SpringBootApplication
 @EnableDiscoveryClient
 public class Run {
@@ -18,7 +20,7 @@ public class Run {
 	
 	
 	@Bean
-	@LoadBalanced//启用负载均衡
+	@LoadBalanced//启用ribbon负载均衡
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
--- src/main/java/com/liqihua/TestController.java
+++ src/main/java/com/liqihua/TestController.java
@@ -1,5 +1,6 @@
 package com.liqihua;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,12 +11,26 @@ import org.springframework.web.client.RestTemplate;
 public class TestController {
 	@Autowired
     RestTemplate restTemplate;
-	
-	
+
+    /**
+     * Hystrix能够使得当调用的服务崩溃或不可用时，调用fallbackMethod指定的方法
+     */
+    @HystrixCommand(fallbackMethod = "hiError")
 	@RequestMapping(value = "/hi")
     public String hi(@RequestParam String name){
 		String result =  restTemplate.getForObject("http://eureka-client-demo1/hi?name="+name, String.class);
 		System.out.println("--- ribbon : result : "+result);
         return result;
     }
+
+    /**
+     * 返回服务调用失败信息
+     * @param name
+     * @return
+     */
+    public String hiError(String name){
+        return name+",service hi is error!";
+    }
+
+
 }
